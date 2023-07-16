f87f55888d3f2c097bd1aa4aa77d8f4c3a99a662
mohoab <138564219+mohoab@users.noreply.github.com>
2023-07-16 12:59:09 +0330

    56

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
-from . import views
+from django.conf  import settings
+from django.conf.urls.static import static 
+
 urlpatterns = [
     path( 'admin/', admin.site.urls) , 
     path( '' , include('root.urls')) ,
-    path('winner/',views.winner)
 ]
 
+urlpatterns +=  static(settings.MEDIA_URL , ducument_root = settings.MEDIA_ROOT)
+urlpatterns +=  static(settings.STATIC_URL , ducument_root = settings.STATIC_ROOT)
